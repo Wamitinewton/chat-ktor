c9b54a5c4f95bcc362720a91de52e2fd81bf765f
Wamitinewton <wamitinewton@gmail.com>
2024-09-03 21:48:48 +0300

    chat socket route configs

--- src/main/kotlin/com/example/plugins/Security.kt
+++ src/main/kotlin/com/example/plugins/Security.kt
@@ -1,23 +1,23 @@
 package com.example.plugins
 
+import com.example.session.ChatSession
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 import io.ktor.server.sessions.*
-import kotlinx.serialization.*
+import io.ktor.util.*
 
 fun Application.configureSecurity() {
-    data class MySession(val count: Int = 0)
     install(Sessions) {
-        cookie<MySession>("MY_SESSION") {
-            cookie.extensions["SameSite"] = "lax"
-        }
+        // each user interacting with this app will have
+        // their chat session stored in a web cookie called "SESSION"
+        cookie<ChatSession>("SESSION")
     }
-    routing {
-        get("/session/increment") {
-                val session = call.sessions.get<MySession>() ?: MySession()
-                call.sessions.set(session.copy(count = session.count + 1))
-                call.respondText("Counter is ${session.count}. Refresh to increment.")
-            }
+// in case of anything in the below interceptor remember to check out on the ApplicationCallPipeline.Features
+    intercept(ApplicationCallPipeline.Plugins) {
+        // sessions are usually
+        // processed in the pipeline
+        if (call.sessions.get<ChatSession>() == null) {
+            val userName = call.parameters["username"] ?: "Guest"
+            call.sessions.set(ChatSession(userName, generateNonce()))
+        }
     }
 }
---
+++ src/main/kotlin/com/example/routes/ChatRoutes.kt
@@ -0,0 +1,44 @@
+package com.example.routes
+
+import com.example.room.MemberAlreadyExists
+import com.example.room.RoomController
+import com.example.session.ChatSession
+import io.ktor.http.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.channels.consumeEach
+
+fun Route.chatSocket(roomController: RoomController) {
+    webSocket("/chat-socket") {
+        val session = call.sessions.get<ChatSession>()
+        if (session == null) {
+            close(CloseReason(CloseReason.Codes.VIOLATED_POLICY, "No session available"))
+            return@webSocket
+        }
+        try {
+            roomController.onJoin(
+                username = session.userName,
+                sessionId = session.sessionId,
+                socket = this
+            )
+            incoming.consumeEach { frame ->
+                if (frame is Frame.Text) {
+                    roomController.sendMessage(
+                        sendUsername = session.userName,
+                        message = frame.readText()
+                    )
+                }
+            }
+        } catch (e: MemberAlreadyExists) {
+            call.respond(HttpStatusCode.Conflict)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            roomController.tryDisconnect(session.userName)
+        }
+
+    }
+}
